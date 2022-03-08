685d9eed635d54d37c31a6f195a17593c7576ed2
Jachym <patryknjachym@gmail.com>
2022-03-08 09:59:48 +0100

    add react router lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { GlobalStyle } from "assets/styles/global-style";
 import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "assets/styles/global-style";
+
+import { theme } from "assets/styles/theme";
+import MainTemplate from "components/layout/main-template/main-template";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-    </>
+      <MainTemplate>
+        <p>chuj</p>
+      </MainTemplate>
+    </ThemeProvider>
   );
 };
 
