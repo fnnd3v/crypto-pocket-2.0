6287ec0bd568b7952fbb32edb87106999adacf10
Jachym <patryknjachym@gmail.com>
2022-03-08 09:37:43 +0100

    project setup

---
+++ src/assets/styles/global-style.tsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *::after, *::before {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    font-family: 'Montserrat', sans-serif;
+    width: 100%;
+    height: 100%;
+  }
+  
+  a, button {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
